40cb3e10b529f546dc8a1e1605258031797a3909
Lusabero <lisaovikyan@gmail.com>
2021-11-14 19:27:55 +0300

    попап

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ const closePopupByClickOverlay = function(event) {
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOverlay);
+
+
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    const nameInfo = document.querySelector('#nameInfo');
+
+    nameInput.value = nameInfo.textContent;
+    const jobInfo = document.querySelector('#jobInfo');
+    jobInput.value = jobInfo.textContent;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
